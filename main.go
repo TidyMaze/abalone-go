1f20b732c5106a392b304a5ef240e66dfa196adb
yrolland <yann.rolland@iadvize.com>
2023-11-01 12:50:44 +0100

    wip

--- main.go
+++ main.go
@@ -7,17 +7,20 @@ import (
 )
 
 func main() {
+	helpers.AssertEqual(Coord2D{X: 3, Y: -3}.To3D(), Coord3D{X: 3, Z: -3})
+	helpers.AssertEqual(Coord2D{X: -3, Y: 3}.To3D(), Coord3D{X: -3, Z: 3})
+
 	fmt.Println("Hello, World!")
 
 	// Create a new game
 	game := NewGame()
-	println(game.Show())
 
-	helpers.AssertEqual(Coord2D{X: 3, Y: -3}.To3D(), Coord3D{X: 3, Z: -3})
-	helpers.AssertEqual(Coord2D{X: -3, Y: 3}.To3D(), Coord3D{X: -3, Z: 3})
+	println("Empty game:")
+	println(game.Show())
 
 	game.SetGrid(Coord2D{X: 3, Y: -3}.To3D(), 1)
 	game.SetGrid(Coord2D{X: -3, Y: 3}.To3D(), 2)
 
+	println("Game with 2 marbles:")
 	println(game.Show())
 }
