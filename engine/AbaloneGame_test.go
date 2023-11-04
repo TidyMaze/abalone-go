fc7b60e1f24e7136ef63a7bdcadc3297a345386a
yrolland <yann.rolland@iadvize.com>
2023-11-04 23:40:23 +0100

    wip

--- engine/AbaloneGame_test.go
+++ engine/AbaloneGame_test.go
@@ -140,3 +140,28 @@ func TestThreePushOne(t *testing.T) {
 
 	helpers.AssertEqual(showGrid(expected.grid), showGrid(gameCopy.grid))
 }
+
+func TestThreePushTwo(t *testing.T) {
+	game := NewGame()
+	game.SetGrid(Coord3D{-2, 2, 0}, 1)
+	game.SetGrid(Coord3D{-1, 1, 0}, 1)
+	game.SetGrid(Coord3D{0, 0, 0}, 1)
+	game.SetGrid(Coord3D{1, -1, 0}, 2)
+	game.SetGrid(Coord3D{2, -2, 0}, 2)
+
+	gameCopy := game.Copy()
+	err := gameCopy.Push(Coord3D{-2, 2, 0}, Right, 3)
+
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	expected := NewGame()
+	expected.SetGrid(Coord3D{-1, 1, 0}, 1)
+	expected.SetGrid(Coord3D{0, 0, 0}, 1)
+	expected.SetGrid(Coord3D{1, -1, 0}, 1)
+	expected.SetGrid(Coord3D{2, -2, 0}, 2)
+	expected.SetGrid(Coord3D{3, -3, 0}, 2)
+
+	helpers.AssertEqual(showGrid(expected.grid), showGrid(gameCopy.grid))
+}
