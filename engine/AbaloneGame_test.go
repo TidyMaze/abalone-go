6550bcc46cfde3e1e74f9dbcd75f4739d32e58d3
yrolland <yann.rolland@iadvize.com>
2023-11-04 23:32:20 +0100

    wip

--- engine/AbaloneGame_test.go
+++ engine/AbaloneGame_test.go
@@ -60,3 +60,24 @@ func TestPushTwoBlocked(t *testing.T) {
 
 	helpers.AssertEqual("not enough marbles to push enemy (got 2, need 3)", err.Error())
 }
+
+func TestPushThree(t *testing.T) {
+	game := NewGame()
+	game.SetGrid(Coord3D{0, 0, 0}, 1)
+	game.SetGrid(Coord3D{1, -1, 0}, 1)
+	game.SetGrid(Coord3D{2, -2, 0}, 1)
+
+	gameCopy := game.Copy()
+	err := gameCopy.Push(Coord3D{0, 0, 0}, Right, 3)
+
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	expected := NewGame()
+	expected.SetGrid(Coord3D{1, -1, 0}, 1)
+	expected.SetGrid(Coord3D{2, -2, 0}, 1)
+	expected.SetGrid(Coord3D{3, -3, 0}, 1)
+
+	helpers.AssertEqual(showGrid(expected.grid), showGrid(gameCopy.grid))
+}
