5bed6df5ca973b71cb0b6528fbf6674ce15317b8
yrolland <yann.rolland@iadvize.com>
2023-11-04 23:48:12 +0100

    wip

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	println("Game with 2 marbles:")
 	println(game.Show())
 
-	game.Push(Coord3D{X: -3, Y: 0, Z: 3}, Right, 1)
+	game.Push(Coord3D{X: -3, Y: 0, Z: 3}, Right)
 
 	println("Game pushed to the right:")
 	println(game.Show())
@@ -38,7 +38,7 @@ func main() {
 			break
 		}
 
-		err := game.Push(*currentMarbleCoord, Right, 1)
+		err := game.Push(*currentMarbleCoord, Right)
 		if err != nil {
 			panic(err)
 		}
