779556d60098175f777f6ec1b13752fada181bf5
yrolland <yann.rolland@iadvize.com>
2023-11-05 13:18:49 +0100

    wip

--- engine/Move.go
+++ engine/Move.go
@@ -1,8 +1,14 @@
 package engine
 
+import "fmt"
+
 type Move interface{}
 
 type PushLine struct {
 	From      Coord3D
 	Direction Direction
+}
+
+func (p PushLine) String() string {
+	return fmt.Sprintf("Action: PushLine, From: %v, Direction: %v", p.From, p.Direction)
 }
