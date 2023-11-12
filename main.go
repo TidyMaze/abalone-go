1f1f1e37ea40b2b4a73a4f03c0568019a06a22a3
yrolland <yann.rolland@iadvize.com>
2023-11-12 13:56:00 +0100

    hidden count

--- main.go
+++ main.go
@@ -112,7 +112,7 @@ func main() {
 	nodesCount := 0
 	allNodes := make([]*network.NNode, 0)
 
-	hiddenNodesCount := 2
+	hiddenNodesCount := 10
 
 	inputNodes := make([]*network.NNode, 0)
 	hiddenNodes := make([]*network.NNode, 0)
